8f53149cd3b5e1998472fe5173854e1b23009087
Dhanushit419 <dhanushit419@gmail.com>
2024-11-06 01:08:43 +0530

    added icon

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from 'react-native-vector-icons'; // Import Ionicons
 import Auth from './screens/auth'; // Import Auth screen
 import Rent from './screens/rent'; // Import Rent screen
-import Add from './screens/add';
+import Add from './screens/add'; // Import Add Book screen
 
 // Create a Tab Navigator
 const Tab = createBottomTabNavigator();
@@ -11,11 +12,27 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Auth Page') {
+              iconName = focused ? 'person-circle' : 'person-circle-outline'; // Change as needed
+            } else if (route.name === 'Rent Page') {
+              iconName = focused ? 'book' : 'book-outline'; // Change as needed
+            } else if (route.name === 'Add Book') {
+              iconName = focused ? 'add-circle' : 'add-circle-outline'; // Change as needed
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={"#164863"} />;
+          },
+        })}
+      >
         <Tab.Screen name="Auth Page" component={Auth} />
         <Tab.Screen name="Rent Page" component={Rent} />
         <Tab.Screen name="Add Book" component={Add} />
-        {/* Add more screens here if needed */}
       </Tab.Navigator>
     </NavigationContainer>
   );
